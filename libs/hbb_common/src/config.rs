27cc617ec66ba67323d65da87a6d92b62cd85edd
jtong1231 <jtong5995@gmail.com>
2024-12-13 03:15:19 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["206.238.42.67"];
-pub const PUBLIC_RS_PUB_KEY: &str = "+reTSQIvvJiTARavNwqJBhyyx+i+d8CBnSpNTe7tqU4=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["206.238.43.212"];
+pub const PUBLIC_RS_PUB_KEY: &str = "QfXEsuXKIvj4xa1Gx2evUEZ5Y7m6buMGxDmmk56g+fo=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
